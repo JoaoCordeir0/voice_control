b49556f54bf68cfcb1414302d7e91392b4e9c4f7
joao.cordeiro <joao.victor@nubbi.com.br>
2024-07-24 16:37:44 -0300

    at

--- create_audio.py
+++ create_audio.py
@@ -7,10 +7,11 @@ def create_audio(string, audio_name):
     tts.save(f'audios/{audio_name}.mp3')
     playsound(f'audios/{audio_name}.mp3')
 
-create_audio('Fala direito porra! Não entendi', 'understand')
+create_audio('Fala direito macaco! Não entendi', 'understand')
 # create_audio('Ocorreu um erro, tente novamente', 'error')
 # create_audio('Ok, estou pausando', 'pause')
 # create_audio('Ok, estou pausando', 'unpause')
 # create_audio('O que tu quer?', 'hello')
 # create_audio('Volume ajustado', 'volume')
 #create_audio('O pc será desligado as 01:00', 'shutdonw')
+# create_audio('', 'yasmyn')
